e272b5614c9283887135048b3257b9199ea3d278
Sahara <saharabegom@gmail.com>
2021-11-09 05:30:55 +0000

    Cont

--- melon_info.py
+++ melon_info.py
@@ -3,7 +3,6 @@
 
 from melons import melon_names, melon_seedlessness, melon_prices
 
-
 def print_melon(name, seedless, price):
     """Print each melon with corresponding attribute information."""
 
@@ -12,7 +11,39 @@ def print_melon(name, seedless, price):
         have_or_have_not = 'do not have'
 
     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
- hello
+ 
 
 for i in melon_names:
     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+print ("                                                                  ")
+print ("==================================================================")
+print ("                                                                  ")
+
+# Edit file to track:
+# flesh color
+# rind color
+# average weight of melons
+
+# from melons import melons.py
+
+# def all_melons(melons):
+#     """ print all melon includind their details """  
+#     for melon_name, 
+
+from melons import melons
+
+
+def print_all_melons(melons):
+    """Print each melon with corresponding attribute information."""
+
+    for melon_name, attributes in melons.items():
+        print(melon_name.upper())
+
+        for attribute, value in attributes.items():
+            print(f'{attribute}: {value}')
+
+        print('===================================')
+
+
+print_all_melons(melons)
